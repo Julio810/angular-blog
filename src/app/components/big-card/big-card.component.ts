da0accc2351f25a10bda3a09c30f8a6daa1b70d7
Julio810 <erison.julio@hotmail.com>
2025-03-14 18:58:22 -0300

    Realizando atualizações

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+  addPhoto: string = "https://blog.bbnd.eu/wp-content/uploads/2024/11/angular19-860x484.png"
+  addDescricao: string = "Confira a versão mais atualizada do Angular"
+  addParagrafo: string = "O Angular 19 chegou trazendo uma série de recursos que prometem transformar a experiência de desenvolvimento e entrega de aplicações web.Entre as principais novidades estão o Incremental Hydration, que otimiza o desempenho de aplicações SSR, o modo de renderização por rota e a evolução do sistema de reatividade com signals e novas APIs estabilizadas."
 
+  constructor () {}
+
+  ngOnInit(): void {
+  }
+  
 }
